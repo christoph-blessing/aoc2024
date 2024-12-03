bb9527256dfaad1af0159a270c3cdfd59875b246
Christoph Blessing <chris24.blessing@gmail.com>
2024-12-03 22:18:34 +0100

    Solve day 3 part 2

--- day03_mull-it-over.zig
+++ day03_mull-it-over.zig
@@ -6,13 +6,24 @@ pub fn main() !void {
 
     var buffered_reader = std.io.bufferedReader(file.reader());
     const reader = buffered_reader.reader();
-    const token = [_]u8{ 'm', 'u', 'l', '(' };
     var sum: i32 = 0;
+    var is_enabled = true;
     while (true) {
-        skip_until(reader, &token) catch |err| switch (err) {
+        if (!is_enabled) {
+            _ = skip_until(reader, &[_][]const u8{"do()"}) catch |err| switch (err) {
+                error.EndOfStream => break,
+                else => return err,
+            };
+            is_enabled = true;
+        }
+        const token = skip_until(reader, &[_][]const u8{ "mul(", "don't()" }) catch |err| switch (err) {
             error.EndOfStream => break,
             else => return err,
         };
+        if (std.mem.eql(u8, token, "don't()")) {
+            is_enabled = false;
+            continue;
+        }
         const number1 = parse_number(reader, ',') catch |err| switch (err) {
             error.EndOfStream => break,
             else => return err,
@@ -26,16 +37,26 @@ pub fn main() !void {
     std.debug.print("The total is: {}", .{sum});
 }
 
-pub fn skip_until(reader: anytype, token: []const u8) !void {
+pub fn skip_until(reader: anytype, tokens: []const []const u8) ![]const u8 {
     var index: u8 = 0;
+    var found: ?[]const u8 = null;
     while (true) {
         const byte = reader.readByte() catch |err| return err;
-        if (byte == token[index]) {
-            index += 1;
-        } else continue;
-        if (index == token.len) {
-            break;
+        var is_active = false;
+        for (tokens) |token| {
+            if (index >= token.len) continue;
+            if (byte != token[index]) continue;
+            if (index == token.len - 1) {
+                found = token;
+                break;
+            }
+            is_active = true;
         }
+        if (found != null) return found.?;
+        if (is_active) {
+            is_active = false;
+            index += 1;
+        } else index = 0;
     }
 }
 
