2ebec7a9391619588ac6a0fc42073f4e2fe695ab
Christoph Blessing <chris24.blessing@gmail.com>
2024-12-24 15:17:01 +0100

    Refactor day 12 part 1

--- day12_garden-groups.zig
+++ day12_garden-groups.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 
-const Location = struct { x: usize, y: usize };
+const Location = struct { x: isize, y: isize };
+const ValidLocation = struct { x: usize, y: usize };
 const Region = struct {
     plant: u8,
-    plots: std.AutoHashMap(Location, bool),
+    plots: std.AutoHashMap(ValidLocation, bool),
     fn init(allocator: std.mem.Allocator, plant: u8) Region {
-        return Region{ .plant = plant, .plots = std.AutoHashMap(Location, bool).init(allocator) };
+        return Region{ .plant = plant, .plots = std.AutoHashMap(ValidLocation, bool).init(allocator) };
     }
     fn deinit(self: *Region) void {
         self.plots.deinit();
@@ -37,10 +38,10 @@ pub fn main() !void {
         regions.deinit();
     }
 
-    const max = Location{ .x = garden.items.len - 1, .y = garden.items[0].len - 1 };
+    const max = ValidLocation{ .x = garden.items.len - 1, .y = garden.items[0].len - 1 };
 
     for (garden.items, 0..) |row, x| for (row, 0..) |_, y| {
-        const plot = Location{ .x = x, .y = y };
+        const plot = ValidLocation{ .x = x, .y = y };
 
         var seen = false;
         for (regions.items) |region| {
@@ -57,38 +58,19 @@ pub fn main() !void {
         try regions.append(region);
     };
 
-    var price: usize = 0;
-    for (regions.items) |region| {
-        var region_perimeter: usize = 0;
+    const part1_price = try part1(allocator, regions.items, max);
 
-        var iterator = region.plots.keyIterator();
-        while (iterator.next()) |plot| {
-            var plot_perimeter: usize = 4;
-
-            const neighbors = try getNeighbors(allocator, plot.*, max);
-            defer allocator.free(neighbors);
-
-            for (neighbors) |neighbor| {
-                if (region.plots.contains(neighbor)) plot_perimeter -= 1;
-            }
-
-            region_perimeter += plot_perimeter;
-        }
-
-        price += region.plots.count() * region_perimeter;
-    }
-
-    std.debug.print("Total price: {}\n", .{price});
+    std.debug.print("Total price: {}\n", .{part1_price});
 }
 
-fn getRegion(allocator: std.mem.Allocator, garden: *[]const []const u8, start: Location, max: Location) !*Region {
+fn getRegion(allocator: std.mem.Allocator, garden: *[]const []const u8, start: ValidLocation, max: ValidLocation) !*Region {
     const region_plant = garden.*[start.x][start.y];
 
-    var queue = std.ArrayList(Location).init(allocator);
+    var queue = std.ArrayList(ValidLocation).init(allocator);
     defer queue.deinit();
     try queue.append(start);
 
-    var seen = std.AutoHashMap(Location, bool).init(allocator);
+    var seen = std.AutoHashMap(ValidLocation, bool).init(allocator);
     defer seen.deinit();
     try seen.put(start, true);
 
@@ -105,20 +87,21 @@ fn getRegion(allocator: std.mem.Allocator, garden: *[]const []const u8, start: L
 
         try region.plots.put(plot, true);
 
-        const neighbors = try getNeighbors(allocator, plot, max);
+        const neighbors = try getNeighbors(allocator, plot);
         defer allocator.free(neighbors);
 
         for (neighbors) |neighbor| {
-            if (seen.contains(neighbor)) continue;
-            try seen.put(neighbor, true);
-            try queue.append(neighbor);
+            const valid_neighbor = validateLocation(neighbor, max) orelse continue;
+            if (seen.contains(valid_neighbor)) continue;
+            try seen.put(valid_neighbor, true);
+            try queue.append(valid_neighbor);
         }
     }
 
     return region;
 }
 
-fn getNeighbors(allocator: std.mem.Allocator, plot: Location, max: Location) ![]const Location {
+fn getNeighbors(allocator: std.mem.Allocator, plot: ValidLocation) ![]const Location {
     var neighbors = std.ArrayList(Location).init(allocator);
     errdefer neighbors.deinit();
 
@@ -126,20 +109,49 @@ fn getNeighbors(allocator: std.mem.Allocator, plot: Location, max: Location) ![]
     for (offsets) |x_offset| for (offsets) |y_offset| {
         if (@abs(x_offset) == @abs(y_offset)) continue;
 
-        const start_x_isize: isize = @intCast(plot.x);
-        const start_y_isize: isize = @intCast(plot.y);
+        const plot_x_isize: isize = @intCast(plot.x);
+        const plot_y_isize: isize = @intCast(plot.y);
 
-        const candidate_x_isize = start_x_isize + x_offset;
-        if (candidate_x_isize < 0 or candidate_x_isize > max.x) continue;
+        const neighbor_x_isize = plot_x_isize + x_offset;
+        const neighbor_y_isize = plot_y_isize + y_offset;
 
-        const candidate_y_isize = start_y_isize + y_offset;
-        if (candidate_y_isize < 0 or candidate_y_isize > max.y) continue;
-
-        const candidate_x: usize = @intCast(candidate_x_isize);
-        const candidate_y: usize = @intCast(candidate_y_isize);
-
-        try neighbors.append(Location{ .x = candidate_x, .y = candidate_y });
+        try neighbors.append(Location{ .x = neighbor_x_isize, .y = neighbor_y_isize });
     };
 
     return neighbors.toOwnedSlice();
+}
+
+fn validateLocation(maybe_location: Location, max: ValidLocation) ?ValidLocation {
+    if (maybe_location.x < 0 or maybe_location.y < 0) return null;
+    if (maybe_location.x > max.x or maybe_location.y > max.y) return null;
+
+    const x: usize = @intCast(maybe_location.x);
+    const y: usize = @intCast(maybe_location.y);
+    return ValidLocation{ .x = x, .y = y };
+}
+
+fn part1(allocator: std.mem.Allocator, regions: []*const Region, max: ValidLocation) !usize {
+    var price: usize = 0;
+    for (regions) |region| {
+        var region_perimeter: usize = 0;
+
+        var iterator = region.plots.keyIterator();
+        while (iterator.next()) |plot| {
+            var plot_perimeter: usize = 4;
+
+            const neighbors = try getNeighbors(allocator, plot.*);
+            defer allocator.free(neighbors);
+
+            for (neighbors) |neighbor| {
+                const valid_neighbor = validateLocation(neighbor, max) orelse continue;
+                if (region.plots.contains(valid_neighbor)) plot_perimeter -= 1;
+            }
+
+            region_perimeter += plot_perimeter;
+        }
+
+        price += region.plots.count() * region_perimeter;
+    }
+
+    return price;
 }
